39c4f5f241c914341f89680f5ca18b861a5c2da7
Dany <89309539+danbenba@users.noreply.github.com>
2026-03-22 12:57:23 +0100

    feat: install @adonisjs/mail, add config and register provider

---
+++ config/mail.ts
@@ -0,0 +1,28 @@
+import env from '#start/env'
+import { defineConfig, transports } from '@adonisjs/mail'
+
+const mailConfig = defineConfig({
+  default: 'resend',
+
+  from: {
+    address: 'noreply@authguard.net',
+    name: 'Faktur',
+  },
+
+  mailers: {
+    resend: transports.resend({
+      key: env.get('RESEND_API_KEY') || '',
+    }),
+
+    smtp: transports.smtp({
+      host: env.get('SMTP_HOST', 'localhost'),
+      port: env.get('SMTP_PORT', '587'),
+    }),
+  },
+})
+
+export default mailConfig
+
+declare module '@adonisjs/mail/types' {
+  export interface MailersList extends InferMailers<typeof mailConfig> {}
+}
