f3e1d9f48804a5555b5f0e5bc3d71289f28f874d
Dany <89309539+danbenba@users.noreply.github.com>
2026-05-16 23:42:19 +0200

    feat(api-v2): augment HttpContext with apiKey, apiClientIp, apiRawBody, apiStartedAt

--- types/http_context.d.ts
+++ types/http_context.d.ts
@@ -1,8 +1,13 @@
 import type Team from '#models/team/team'
+import type ApiKey from '#models/api/api_key'
 
 declare module '@adonisjs/core/http' {
   interface HttpContext {
     dek?: Buffer
     team?: Team
+    apiKey?: ApiKey
+    apiClientIp?: string
+    apiRawBody?: string
+    apiStartedAt?: number
   }
 }
