67e8403b004e92ec4ed7d596c9c129a9e95696d8
Dany <89309539+danbenba@users.noreply.github.com>
2026-05-18 21:41:07 +0200

    chore(backend): delete v2 meta/pipeline/index/not_found route files (api_v2 folder fully gone)

--- start/routes/api_v2/_not_found.ts
+++
@@ -1,6 +0,0 @@
-import router from '@adonisjs/core/services/router'
-import { API_V2_PREFIX } from '#start/routes/api_v2/_pipeline'
-
-const NotFound = () => import('#controllers/api_v2/not_found')
-
-router.any(`${API_V2_PREFIX}/*`, [NotFound, 'handle']).as('apiV2.notFound')
