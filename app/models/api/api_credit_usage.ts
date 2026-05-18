12701fa29f8ae1c222124bf21cc960abe5df9f1a
Dany <89309539+danbenba@users.noreply.github.com>
2026-05-18 20:41:25 +0200

    feat(backend): ApiCreditUsage Lucid model

---
+++ app/models/api/api_credit_usage.ts
@@ -0,0 +1,39 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class ApiCreditUsage extends BaseModel {
+  public static table = 'api_credit_usage'
+
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare teamId: string
+
+  @column()
+  declare userId: string | null
+
+  @column.date()
+  declare day: DateTime
+
+  @column.date()
+  declare weekStart: DateTime
+
+  @column()
+  declare dailyCount: number
+
+  @column()
+  declare weeklyCount: number
+
+  @column.dateTime()
+  declare lastMinuteAt: DateTime | null
+
+  @column()
+  declare minuteCount: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
