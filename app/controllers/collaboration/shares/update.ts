ec38641a13f35bfac16e3c8b97a9f9d618d8be9d
Dany <89309539+danbenba@users.noreply.github.com>
2026-06-09 21:33:47 +0200

    refactor(collab): move the permission-changed emit into a websocket service helper

--- app/controllers/collaboration/shares/update.ts
+++ app/controllers/collaboration/shares/update.ts
@@ -1,7 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import DocumentShare from '#models/collaboration/document_share'
 import { updateShareValidator } from '#validators/collaboration_validator'
-import { getSocketServer } from '#services/collaboration/websocket_service'
+import { updateCollaboratorPermission } from '#services/collaboration/websocket_service'
 
 export default class Update {
   async handle(ctx: HttpContext) {
@@ -30,19 +30,12 @@ export default class Update {
     await share.save()
 
     if (share.sharedWithUserId) {
-      const io = getSocketServer()
-      if (io) {
-        const roomKey = `${share.documentType}:${share.documentId}`
-        const collabNs = io.of('/collaboration')
-        const sockets = await collabNs.in(roomKey).fetchSockets()
-        for (const s of sockets) {
-          if ((s as any).userId === share.sharedWithUserId) {
-            s.emit('permission-changed', {
-              permission: share.permission,
-            })
-          }
-        }
-      }
+      await updateCollaboratorPermission(
+        share.documentType,
+        share.documentId,
+        share.sharedWithUserId,
+        share.permission
+      )
     }
 
     return response.ok({
